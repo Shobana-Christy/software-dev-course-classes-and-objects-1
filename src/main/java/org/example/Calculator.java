4fb1b936f1b24aaa2dde671c8a9eb9f2b60f4f7a
Shobana-Christy <shobichristy07@gmail.com>
2025-12-23 17:18:42 -0600

    practiced classes and object-1

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -1,11 +1,32 @@
 package org.example;
 
 public class Calculator {
-    // Add a static function called add that takes two int parameters and returns their sum (as an int)
+    // Add a static function called add that takes two int parameters and
+    // returns their sum (as an int)
 
-    // Add a static function called subtract that takes two int parameters and returns their difference (as an int)
+    public static int add(int numOne, int numTwo){
+        int sum = numOne + numTwo;
+        return sum;
+    }
 
-    // Add a static function called multiply that takes two int parameters and returns their product (as an int)
+    // Add a static function called subtract that takes two int parameters and
+    // returns their difference (as an int)
+    public static int subtract(int numOne,int numTwo){
+        int difference = numOne - numTwo;
+        return difference;
+    }
 
-    // Add a static function called divide that takes two int parameters and returns their quotient (as an int)
+    // Add a static function called multiply that takes two int parameters and
+    // returns their product (as an int)
+    public static int multiply(int numOne,int numTwo){
+        int product = numOne * numTwo;
+        return product;
+    }
+
+    // Add a static function called divide that takes two int parameters and
+    // returns their quotient (as an int)
+    public static int divide(int numOne,int numTwo){
+        int quotient = numOne / numTwo;
+        return quotient;
+    }
 }
--- src/main/java/org/example/StringUtilities.java
+++ src/main/java/org/example/StringUtilities.java
@@ -3,14 +3,38 @@ package org.example;
 public class StringUtilities {
     // Refer to Java Basics 1 for common string methods
 
-    // Add a static function called shortString that takes a String parameter and returns true if the string has fewer than 5
-    // characters
+    // Add a static function called shortString that takes a String parameter and
+    // returns true if the string has fewer than 5 characters
 
-    // Add a static function called firstLetter that takes a String parameter and returns the first character of the string
+    public static boolean shortString(String str) {
+        return str != null && str.length() < 5;
 
-    // Add a static function called censorAsparagus that takes a String parameter and returns the string with all instances of
+    }
+
+    // Add a static function called firstLetter that takes a String parameter and
+    // returns the first character of the string
+    public static char firstLetter(String str) {
+        return str.charAt(0);
+    }
+
+    // Add a static function called censorAsparagus that takes a String parameter and
+    // returns the string with all instances of
     // "asparagus" (lowercase only) replaced with 4 stars: "****"
+    public static String censorAsparagus(String str) {
+        return  str.replace("asparagus", "****");
+    }
 
-    // Add a static function called bigger that takes two String parameters and returns the longer of the two strings. If the
-    // strings are the same length, return the first string.
+// Add a static function called bigger that takes two String parameters and
+// returns the longer of the two strings. If the
+// strings are the same length, return the first string.
+
+    public static String bigger(String strOne, String strTwo) {
+        if (strOne.length() >= strTwo.length()) {
+            return strOne;
+        }
+        return strTwo;
+
+    }
+
 }
+
